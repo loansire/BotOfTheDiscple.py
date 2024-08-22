fc929b4367cfd15693e8ddc5ad2b3f9a3cb768a3
loansire <loan.sire@corys.fr>
2024-08-23 01:22:35 +0200

    htmltopng for testing

--- PythonProject/Source/Utils/HTMLToPNG.py
+++ PythonProject/Source/Utils/HTMLToPNG.py
@@ -17,8 +17,20 @@ def html_to_png(html_path, output_path):
     # Charger le fichier HTML
     driver.get(f"file:///{html_path}")
 
+    # Fixer la taille de la fenêtre à 1920x1080 pixels
+    driver.set_window_size(1920, 1080)
+    # Fixer la position de la fenêtre à (0, 0)
+    driver.set_window_position(0, 0)
+
     # Attendre 3 secondes pour laisser toutes les animations CSS se terminer
     time.sleep(3)
+
+    # Obtenir les dimensions et la position de la fenêtre
+    window_rect = driver.get_window_rect()
+    print(f"Largeur de la fenêtre: {window_rect['width']} pixels")
+    print(f"Hauteur de la fenêtre: {window_rect['height']} pixels")
+    print(f"Position X de la fenêtre: {window_rect['x']} pixels")
+    print(f"Position Y de la fenêtre: {window_rect['y']} pixels")
 
     # Prendre une capture d'écran entière de la page
     screenshot = driver.get_screenshot_as_png()
