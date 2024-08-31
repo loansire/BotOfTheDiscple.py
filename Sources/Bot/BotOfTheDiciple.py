7b58e617eee485efef6959a93e9b6dfbf96aaff5
loansire <loan.sire@corys.fr>
2024-09-01 01:35:21 +0200

    Update BotOfTheDiciple.py

--- Sources/Bot/BotOfTheDiciple.py
+++ Sources/Bot/BotOfTheDiciple.py
@@ -32,10 +32,10 @@ async def on_ready():
     # await check_messages()
 
     # Actualisation du Secteur oublié du jour lorsque le bot s'initialise
-    GenerateActivity(False)
+    # GenerateActivity(False)
 
     # Démarrer la tâche de mise à jour quotidienne à 19h
-    daily_update.start()
+    # daily_update.start()
 
 
 # Enregistrement des commandes slash
