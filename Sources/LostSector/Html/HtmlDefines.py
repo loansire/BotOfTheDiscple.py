cd5bd9e4abebde10cb8a9b8d094b2cec556c752c
loansire <loan.sire@corys.fr>
2024-08-31 20:27:37 +0200

    Update HtmlDefines.py

--- Sources/LostSector/Html/HtmlDefines.py
+++ Sources/LostSector/Html/HtmlDefines.py
@@ -13,6 +13,6 @@ E_ID_POWER = "power_lever_expert"
 M_ID_POWER = "power_lever_master"
 
 #Templates
-T_SHIELDS_PATH = Config.RessourcePath(r"FunctionsTemplates\Shields.txt")
-T_CHAMP_PATH = Config.RessourcePath(r"FunctionsTemplates\Champs.txt")
-T_MODIFIER_PATH = Config.RessourcePath(r"FunctionsTemplates\Modifiers.txt")
+T_SHIELDS_PATH = Config.RessourcePath(r"FunctionsTemplates/Shields.txt")
+T_CHAMP_PATH = Config.RessourcePath(r"FunctionsTemplates/Champs.txt")
+T_MODIFIER_PATH = Config.RessourcePath(r"FunctionsTemplates/Modifiers.txt")
