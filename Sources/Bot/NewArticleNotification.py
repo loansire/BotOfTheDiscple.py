117134fbbddd0552904b073a170d2825ff9d8aab
loansire <loan.sire@corys.fr>
2024-11-01 03:11:12 +0100

    Update NewArticleNotification.py

--- Sources/Bot/NewArticleNotification.py
+++ Sources/Bot/NewArticleNotification.py
@@ -28,11 +28,7 @@ async def NewArticleTest():
     try:
         # Récupérer les derniers articles TWID et Destiny 2 Update
         twid_item, _ = await get_latest_article_by_keyword('en', 'twid')
-        keywords = ['destiny_2_update', 'destiny_update']
-        for keyword in keywords:
-            update_item, _ = await get_latest_article_by_keyword('en', keyword)
-            if update_item:  # Si un article est trouvé, on arrête la boucle
-                break
+        update_item, _ = await get_latest_article_by_keyword('en', 'destiny_update')
 
         # Charger les données actuelles depuis le fichier JSON
         news_alerts = await load_news_alerts()
