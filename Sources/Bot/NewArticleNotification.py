ed6842c7a7a67a80d7bf8b465f4e1edd20faf2b5
loansire <loan.sire@corys.fr>
2024-11-01 05:31:46 +0100

    Update NewArticleNotification.py

--- Sources/Bot/NewArticleNotification.py
+++ Sources/Bot/NewArticleNotification.py
@@ -20,7 +20,7 @@ async def load_news_alerts():
 # Sauvegarder les alertes mises à jour
 async def save_news_alerts(news_alerts):
     with open('Ressources/AlertDatabase/news_alert.json', 'w') as file:
-        json.dump(news_alerts, file, indent=4)
+        json.dump(news_alerts, file, indent=2)
 
 
 async def NewArticleTest():
