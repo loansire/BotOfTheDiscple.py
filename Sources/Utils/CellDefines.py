49ef488b759305d4e1b0db6b982719ff66963c03
loansire <loan.sire@corys.fr>
2024-11-06 17:12:03 +0100

    Update CellDefines.py

--- Sources/Utils/CellDefines.py
+++ Sources/Utils/CellDefines.py
@@ -1,20 +1,49 @@
-﻿CELL_NAME = "A2"
-CELL_SURCHARGE_1 = "B2"
-CELL_SURCHARGE_2 = "C2"
-CELL_POWER_E = "D2"
-CELL_POWER_M = "E2"
+﻿
+#region  Cell SecteurOublié
+CELL_NAME = "A2"
+CELL_HASH_E = "B2"
+CELL_HASH_M = "C2"
+CELL_SURCHARGE_1 = "D2"
+CELL_SURCHARGE_2 = "E2"
+CELL_POWER_E = "F2"
+CELL_POWER_M = "G2"
 
 #Cell champs Expert
-CELL_SHIELD_E = {"Solaires" : "F2", "Abyssaux" : "G2", "Cryo-électriques" : "H2", "Stasiques" : "I2", "Filobscures" : "J2"}
+CELL_SHIELD_E = {"Solaires" : "H2", "Abyssaux" : "I2", "Cryo-électriques" : "J2", "Stasiques" : "K2", "Filobscures" : "L2"}
 
 #Cell shield Expert
-CELL_CHAMP_E = {"Brise-bouclier" : "K2", "Perturbation" : "L2","Chancellement" : "M2"}
+CELL_CHAMP_E = {"Brise-bouclier" : "M2", "Perturbation" : "N2","Chancellement" : "O2"}
 
 #Cell Champs Maitrise
-CELL_SHIELD_M = {"Solaires" : "N2", "Abyssaux" : "O2", "Cryo-électriques" : "P2", "Stasiques" : "Q2", "Filobscures" : "R2"}
+CELL_SHIELD_M = {"Solaires" : "P2", "Abyssaux" : "Q2", "Cryo-électriques" : "R2", "Stasiques" : "S2", "Filobscures" : "T2"}
 
 #Cell Shields Maitrise
-CELL_CHAMP_M = {"Brise-bouclier" : "S2", "Perturbation" : "T2","Chancellement" : "U2"} 
+CELL_CHAMP_M = {"Brise-bouclier" : "U2", "Perturbation" : "V2","Chancellement" : "W2"}
+
+#Cell Arme 1
+CELL_WEAPON_NAME_1 = "B4"
+CELL_WEAPON_HASH_1 = "C4"
+CELL_WEAPON_LINK_FR_1 = "D4"
+CELL_WEAPON_LINK_EN_1 = "E4"
+
+#Cell Arme 2
+CELL_WEAPON_NAME_2 = "F4"
+CELL_WEAPON_HASH_2 = "G4"
+CELL_WEAPON_LINK_FR_2 = "H4"
+CELL_WEAPON_LINK_EN_2 = "I4"
+
+#Cell Arme 3
+CELL_WEAPON_NAME_3 = "J4"
+CELL_WEAPON_HASH_3 = "K4"
+CELL_WEAPON_LINK_FR_3 = "L4"
+CELL_WEAPON_LINK_EN_3 = "M4"
+
+#Cell Arme 4
+CELL_WEAPON_NAME_4 = "N4"
+CELL_WEAPON_HASH_4 = "O4"
+CELL_WEAPON_LINK_FR_4 = "P4"
+CELL_WEAPON_LINK_EN_4 = "Q4"
+#endregion
 
 CELL_HOUR = "B7"
 
