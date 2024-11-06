037f579a9cf7ba9b674b0ab84e295ce0266d17dd
loansire <loan.sire@corys.fr>
2024-11-06 17:21:05 +0100

    Update Dictionnary.py
    Correction du terme "perturbé" en "surchargé"

--- Sources/Utils/Dictionnary.py
+++ Sources/Utils/Dictionnary.py
@@ -1,9 +1,9 @@
 ﻿SURCHARGE_TRANSLATION = {"Abyssal" : "Abyssaux", "Cryo" : "Cryo-électriques", "Abyssale" : "Abyssaux", "Solaire" : "Solaires"}
 
-BREAKER_TYPES = {"Brise-bouclier", "Chancellement", "Overload"}
+BREAKER_TYPES = {"Brise-bouclier", "Chancellement", "Perturbation"}
 
 SHIELD_TYPES = {"Stasiques", "Filobscures", "Solaires", "Cryo-électriques", "Abyssaux"}
 
-FINAL_TRANSLATIONS_HTML = {"Brise-bouclier" : "Bloqueurs", "Chancellement" : "Implacables", "Overload" : "Surchargés", "Stase" : "Stasiques", "Filobscure" : "Filobscures"}
+FINAL_TRANSLATIONS_HTML = {"Brise-bouclier" : "Bloqueurs", "Chancellement" : "Implacables", "Perturbation" : "Surchargés", "Stase" : "Stasiques", "Filobscure" : "Filobscures"}
 
-FINAL_TRANSLATIONS_BOT = {"Brise-bouclier" : "Bloqueur", "Chancellement" : "Implacable", "Overload" : "Overload", "Stasiques" : "Stase", "Filobscures" : "Filobscure", "Abyssaux" : "Abyssale", "Cryo-électriques" : "Cryo", "Solaires" : "Solaire"}
+FINAL_TRANSLATIONS_BOT = {"Brise-bouclier" : "Bloqueur", "Chancellement" : "Implacable", "Perturbation" : "Surcharge", "Stasiques" : "Stase", "Filobscures" : "Filobscure", "Abyssaux" : "Abyssale", "Cryo-électriques" : "Cryo", "Solaires" : "Solaire"}
