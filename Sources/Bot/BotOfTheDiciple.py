d82a1d437a1e7a4753818c4f33da57c4419410db
loansire <loan.sire@corys.fr>
2024-11-22 03:16:28 +0100

    Update BotOfTheDiciple.py

--- Sources/Bot/BotOfTheDiciple.py
+++ Sources/Bot/BotOfTheDiciple.py
@@ -488,7 +488,7 @@ async def recurring_update():
 
 async def main():
     async with bot:
-        await bot.start('MTI3MDM1NzgzMDM3Njg4MjI5OQ.Gq46tf.AIDE8gaTf1WAymorihg_mUh6yTCbWRiB7ylF7Q')
+        await bot.start('MTI3MDAyMjIyMTc4MzQ5ODg0NA.GzQ5Zl.MuQAcDfdlTAqifThbcML96gXcwdM9gmFlZ5xfI')
 
 
 # Démarrage de l'événement principal
